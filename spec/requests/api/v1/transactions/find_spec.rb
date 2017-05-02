8bb73b432ef862aa7a093d5a64daf5b0c6cf3800
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-05-02 14:49:47 -0600

    fixes credit_cart_typo

--- spec/requests/api/v1/transactions/find_spec.rb
+++ spec/requests/api/v1/transactions/find_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe 'Transaction Find API' do
   it 'finds a transaction by any associated attributes' do
     transaction = create(:transaction)
-    cc_number = transaction.credit_cart_number
+    cc_number = transaction.credit_card_number
 
-    get "/api/v1/transactions/find?credit_cart_number=#{cc_number}"
+    get "/api/v1/transactions/find?credit_card_number=#{cc_number}"
     returned_transaction = JSON.parse(response.body)
 
     expect(response).to be_success
@@ -16,10 +16,10 @@ describe 'Transaction Find API' do
 
   it 'finds all transactions by any associated attributes' do
     transaction_1, transaction_2 = create_list(:transaction, 2)
-    transaction_3 = create(:transaction, credit_cart_number: "4580251236515201")
-    cc_number = transaction_1.credit_cart_number
+    transaction_3 = create(:transaction, credit_card_number: "4580251236515201")
+    cc_number = transaction_1.credit_card_number
 
-    get "/api/v1/transactions/find_all?credit_cart_number=#{cc_number}"
+    get "/api/v1/transactions/find_all?credit_card_number=#{cc_number}"
     json = JSON.parse(response.body)
 
     expect(response).to be_success
